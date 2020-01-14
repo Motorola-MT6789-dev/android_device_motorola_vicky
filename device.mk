77a6a23605d0cdcd69573fe6aaf7ec61b799aabb
Leo Liou <leoliou@google.com>
2020-01-14 17:59:51 +0800

    vicky: Inherit the virtual a/b config
    For devices launching with virtual A/B, inherit the virtual A/B
    device base configuration.
    
    Bug: 146466421
    Bug: 149155051
    Test: boot to home, check system property of virtual_ab
    Test: Run full storage-qa test
    Change-Id: I8ebbbe897dc9278f43e37a6a9599b89d68419b27
    Signed-off-by: Leo Liou <leoliou@google.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Enable virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
