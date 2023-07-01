2c09fd8523e13a1513034b752e096ebf4716f99e
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-07-01 17:25:49 +0700

    vicky: Enable updateable APEX
    Change-Id: Icd14570f0029853debe1977fc00a084d2db85d7c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,9 @@
 DEVICE_PATH := device/motorola/vicky
 KERNEL_PATH := device/motorola/vicky-kernel
 
+# APEX
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
